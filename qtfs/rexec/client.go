bb371be7fbf5a9c8a5d19da2eab2f705fa11a6a0
yangxin <245051644@qq.com>
2022-11-26 11:08:01 +0800

    add local&remote pid maps
    Signed-off-by: yangxin <245051644@qq.com>

--- qtfs/rexec/client.go
+++ qtfs/rexec/client.go
@@ -6,12 +6,78 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/docker/libchan"
 	"github.com/docker/libchan/spdy"
+	"golang.org/x/sys/unix"
 )
+
+const (
+	rexecPidDir = "/var/run/rexec/pids"
+)
+
+var pidPath string
+var pidFile *os.File
+
+func newPidFile(pid int, lpid int) error {
+	err := os.MkdirAll(rexecPidDir, 0700)
+	if err != nil {
+		return err
+	}
+	pidPath = filepath.Join(rexecPidDir, strconv.Itoa(lpid))
+
+	pidFile = nil
+	// create pid file and write remote pid into it
+	pidFile, err = os.OpenFile(pidPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Printf("failed to open %s with %s", pidPath, err)
+		return err
+	}
+	_, err = pidFile.WriteString(strconv.Itoa(pid))
+	if err != nil {
+		os.RemoveAll(pidPath)
+		return err
+	}
+	log.Printf("new pid file(%d), with pid(%d)\n", lpid, pid)
+	// Flock it to mark it inuse
+	syscall.Flock(int(pidFile.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+
+	return nil
+}
+
+func removePidFile() {
+	if pidFile != nil {
+		syscall.Flock(int(pidFile.Fd()), syscall.LOCK_UN)
+		pidFile.Close()
+	}
+
+	os.RemoveAll(pidPath)
+}
+
+func cleanRedundantPidFile() {
+	filepath.Walk(rexecPidDir, func(path string, info os.FileInfo, err error) error {
+		f, err := os.Open(path)
+		if err != nil {
+			// open failed, just skip
+			return err
+		}
+		// Flock it to check if it's locked
+		if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+			// file locked, just skip
+			return nil
+		}
+		// file unlocked, it's redundant, just remove it
+		syscall.Flock(int(f.Fd()), syscall.LOCK_UN)
+		f.Close()
+		os.RemoveAll(path)
+
+		return nil
+	})
+}
 
 func sigHandler() {
 	c := make(chan os.Signal)
@@ -25,6 +91,13 @@ func sigHandler() {
 	}
 }
 
+func SetParentDeathSignal(sig uintptr) error {
+	if err := unix.Prctl(unix.PR_SET_PDEATHSIG, sig, 0, 0, 0); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("usage: <command> [<arg> ]")
@@ -34,7 +107,11 @@ func main() {
 	var err error
 
 	go sigHandler()
-	na , err := parseNetAddr()
+
+	if err := SetParentDeathSignal(uintptr(syscall.SIGHUP)); err != nil {
+		log.Printf("Failed to set Parent Death Signal:%s", err.Error())
+	}
+	na, err := parseNetAddr()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,6 +124,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	cleanRedundantPidFile()
 
 	p, err := spdy.NewSpdyStreamProvider(client, false)
 	if err != nil {
@@ -66,7 +144,7 @@ func main() {
 		Stdin:      os.Stdin,
 		Stdout:     os.Stdout,
 		Stderr:     os.Stderr,
-		Env:		append([]string{}, os.Environ()...),
+		Env:        append([]string{}, os.Environ()...),
 		StatusChan: remoteSender,
 	}
 
@@ -77,7 +155,7 @@ func main() {
 
 	retryCnt := 3
 	// 1. get pid from response
-	time.Sleep(5*time.Millisecond)
+	time.Sleep(5 * time.Millisecond)
 	response := &CommandResponse{}
 retry:
 	err = receiver.Receive(response)
@@ -89,6 +167,12 @@ retry:
 		}
 		log.Fatal(err)
 	}
+	pid := response.Pid
+	lpid := os.Getpid()
+	log.Printf("create pidFile for %d:%d\n", pid, lpid)
+	if err := newPidFile(pid, lpid); err != nil {
+		log.Fatal("failed to create pidFile for %d:%d - %s\n", pid, lpid, err)
+	}
 	retryCnt = 3
 	// 2. get return status from response
 retry2:
@@ -99,8 +183,9 @@ retry2:
 			retryCnt--
 			goto retry2
 		}
-		log.Fatal(err)
+		log.Print(err)
 	}
 
+	removePidFile()
 	os.Exit(response.Status)
 }
